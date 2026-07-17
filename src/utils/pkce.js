f8647d432a9ecb75e9882abb480de17cad03601e
Lex Graves <acg6810@gmail.com>
2026-07-17 16:12:50 -0500

    created pkce password exchange

---
+++ src/utils/pkce.js
@@ -0,0 +1,21 @@
+function generateCodeVerifier(length = 128) {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return Array.from(values)
+    .map((v) => possible[v % possible.length])
+    .join('');
+}
+
+async function generateCodeChallenge(verifier) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(verifier);
+  const digest = await crypto.subtle.digest('SHA-256', data);
+
+  // Base64url-encode the hash
+  return btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+}
+
+export { generateCodeVerifier, generateCodeChallenge };
