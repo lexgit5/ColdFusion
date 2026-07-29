0428505d9b0c7d91762337abad4c9cd4bfd6730a
Lex Graves <acg6810@gmail.com>
2026-07-29 18:45:31 -0500

    made wrangler stable, and made fullscreen

--- .wrangler/tmp/dev-DMfmna/functionsWorker-0.6267370943173336.js
+++ .wrangler/tmp/dev-DMfmna/functionsWorker-0.6267370943173336.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/pages-Bc6Xo0/functionsWorker-0.5447456095021825.mjs
+// .wrangler/tmp/pages-FZ9FlU/functionsWorker-0.6267370943173336.mjs
 var __defProp2 = Object.defineProperty;
 var __name2 = /* @__PURE__ */ __name((target, value) => __defProp2(target, "name", { value, configurable: true }), "__name");
 async function onRequest(context) {
@@ -715,7 +715,7 @@ var jsonError2 = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default2 = jsonError2;
 
-// .wrangler/tmp/bundle-BEzrMm/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-osaJMy/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__2 = [
   middleware_ensure_req_body_drained_default2,
   middleware_miniflare3_json_error_default2
@@ -747,7 +747,7 @@ function __facade_invoke__2(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__2, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-BEzrMm/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-osaJMy/middleware-loader.entry.ts
 var __Facade_ScheduledController__2 = class ___Facade_ScheduledController__2 {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
@@ -849,4 +849,4 @@ export {
   __INTERNAL_WRANGLER_MIDDLEWARE__2 as __INTERNAL_WRANGLER_MIDDLEWARE__,
   middleware_loader_entry_default2 as default
 };
-//# sourceMappingURL=functionsWorker-0.5447456095021825.js.map
+//# sourceMappingURL=functionsWorker-0.6267370943173336.js.map
